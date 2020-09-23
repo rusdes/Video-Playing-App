6dc4904f830eb419423dfd70643997a50cd86463
Rushil Desai <rushildesai01@gmail.com>
2020-09-23 05:09:43 -0400

    added api functionality

--- App/Screens/navigation/search_page/search_page.js
+++ App/Screens/navigation/search_page/search_page.js
@@ -13,13 +13,43 @@ import {
   FlatList,
 } from 'react-native';
 
+import api_call from '../../../api_call';
+
 export default class SearchPage extends Component {
+  state = {
+    query: '',
+    videos: [],
+    selectedVideo: null,
+  };
+
+  onChangeSearch = (text) => {
+    this.setState({query: text});
+  };
+
+  searchYT = async () => {
+    const response = await api_call.get('./search', {
+      params: {
+        q: this.state.query,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+    });
+    console.log(this.state.videos);
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Appbar.Header>
           <Appbar.Content title="Search Page" />
         </Appbar.Header>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={this.onChangeSearch}
+          value={this.state.query}
+          onBlur={this.searchYT}
+        />
       </View>
     );
   }
---
+++ App/api_call.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAI-YIuloLyWARHFN-lMIzTwFfE9Jj00Hk';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3/',
+  params: {
+    part: 'snippet',
+    maxResults: 10,
+    key: KEY,
+  },
+});
